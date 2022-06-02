f6f79a542d386b933bb7c9b385eff5073ba82ded
sudhanshu <kaswansudhanshu@gmail.com>
2022-06-02 15:11:46 +0530

    add media queries

--- src/components/product/productsList.js
+++ src/components/product/productsList.js
@@ -1,71 +1,24 @@
 import CardDetails from "../main/Cards";
+import styles from "../../styles/productsList.module.css";
 
 const ProductsList = ({ products, getNextPage, getPrevPage }) => {
   return (
-    <div className="productsList" style={styles.productsList}>
-      <div className="productsContainer" style={styles.productsContainer}>
+    <div className={styles.productsList}>
+      <div className={styles.productsContainer}>
         {products?.map((product, index) => {
           return <CardDetails product={product} key={"prod-" + index} />;
         })}
       </div>
-      <div style={styles.pageBtns}>
-        <button
-          className="prevBtn"
-          style={styles.prevBtn}
-          onClick={getPrevPage}
-        >
+      <div className={styles.pageBtns}>
+        <button className={styles.prevBtn} onClick={getPrevPage}>
           Prev.
         </button>
-        <button
-          className="nextBtn"
-          style={styles.nextBtn}
-          onClick={getNextPage}
-        >
+        <button className={styles.nextBtn} onClick={getNextPage}>
           Next.
         </button>
       </div>
     </div>
   );
-};
-
-const styles = {
-  productsContainer: {
-    display: "grid",
-    gridTemplateColumns: "1fr 1fr 1fr 1fr",
-    columnGap: "5rem",
-    rowGap: "2rem",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  productsList: {
-    padding: "1rem 0rem",
-    position: "relative",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  pageBtns: {
-    marginTop: "1.5rem",
-  },
-  prevBtn: {
-    position: "absolute",
-    left: "50px",
-    bottom: "10px",
-    background: "transparent",
-    border: "1px solid black",
-    borderRadius: "10px",
-    cursor: "pointer",
-  },
-  nextBtn: {
-    position: "absolute",
-    right: "50px",
-    bottom: "10px",
-    background: "transparent",
-    cursor: "pointer",
-    border: "1px solid black",
-    borderRadius: "10px",
-  },
 };
 
 export default ProductsList;
